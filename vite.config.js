245e3f1c574a10256fbef3b72f6d0ea25ad05dfa
kcccendana <kc_telyn@yahoo.com>
2025-04-06 09:56:30 +0900

    initial files and folders

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        movies: resolve(__dirname, "src/movies/index.html"),
+        mylist: resolve(__dirname, "src/mylist/index.html"),
+        // product: resolve(__dirname, "src/product_pages/index.html"),
+        // product_listing: resolve(__dirname, "src/product_listing/index.html"),
+      },
+    },
+  },
+});
